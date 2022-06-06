975e0c071751bfc1540076e732b48ac0c2b947df
sandydasari <96286740+sandydasari@users.noreply.github.com>
2022-06-06 17:56:36 +0530

    Update test_extract_details.py

--- test_extract_details.py
+++ test_extract_details.py
@@ -5,9 +5,9 @@ import os
 
 class MyTestCase(unittest.TestCase):
     def test_details(self):
+        os.system("python -Wignore extract_details.py")
         file1 = "C:/Users/HP/nlptools/output.json"
         file2 = "C:/Users/HP/nlptools/check_output.json"
-        os.system("python -Wignore extract_details.py")
         if filecmp.cmp(file1, file2):
             print(str(file1) + " is correct")
         else:
